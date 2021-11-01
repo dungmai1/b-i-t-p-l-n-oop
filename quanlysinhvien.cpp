0a2468891782f6f927eafb57c6c63e0e7c4e78ec
dungmai1 <40397060+dungmai1@users.noreply.github.com>
2021-11-01 20:00:52 +0700

    Add files via upload

--- quanlysinhvien.cpp
+++ quanlysinhvien.cpp
@@ -13,13 +13,12 @@ class nguoi : public ngaysinh
     private:
         int msv;
         string ten;
-        ngaysinh ns;
         string gt;
+        int tuoi;
     public:
         void nhap_nguoi();
         void xuat_nguoi();
-        void nhap_ns();
-	    void xuat_ns();
+        void timkiem_msv();
 };
 class diem : public nguoi
 {
@@ -45,34 +44,38 @@ class hocluc: public diem
  		void hocluc1();
  		void sapxephocluc();
 };
-void nguoi::nhap_ns()
-{
-	char ch;
-	cout<<"Nhap vao ngay, thang, nam (dd/mm/yyyy):  ";
-	cin>>ngay>>thang>>nam;
-}
-void nguoi::xuat_ns()
-{
-	cout<<ngay<<"/"<<thang<<"/"<<nam<<endl;
-}
 void nguoi::nhap_nguoi(){
+	cin.ignore();
 	cout<<"\nNhap ho ten sinh vien: ";
 	getline(cin,ten);
-	fflush(stdin);
 	cout<<"\nNhap ma so sinh vien: ";
 	cin>>msv;
 	fflush(stdin);
 	cout<<"\nNhap gioi tinh: ";
 	getline(cin,gt);
+	cout<<"\nNhap vao ngay, thang, nam (dd/mm/yyyy):  ";
+	cin>>ngay>>thang>>nam;
+	cout<<"\nNhap tuoi: ";
+	cin>>tuoi;
 }
 void nguoi::xuat_nguoi(){
-	cout<<"\nThong tin sinh vien ban vua nhap la: ";
-	cout<<"\nHo va ten: "<<ten;
-	cout<<"\nMa sinh vien: "<<msv;
-	cout<<"\nGioi tinh: "<<gt;
+	int n;
+	cout <<"\n\STT\tHo va ten\tMa sinh vien\tGioi tinh\tNgay/Thang/Nam\tTuoi\tDTB\tDRL\tDC\tHoc luc";
+	for(int i = 0; i < n; i++) {
+        cout<<"\n"<< i + 1;
+        cout<<"\t"<<ten;
+        cout<<"\t\t"<<msv;
+        cout<<"\t\t"<<gt;
+        cout<<"\t\t"<<ngay<<"/"<<thang<<"/"<<nam;
+        cout<<"\t\t"<<tuoi;
+	}
+}
+void nguoi::timkiem_msv(){
 }
 int main()
 {
+	int n,x;
+	nguoi ns;
 	cout<<"\n***********************MENU****************************";
     cout<<"\n*                 QUAN LY SINH VIEN                   *";
     cout<<"\n*                                                     *";
@@ -84,9 +87,25 @@ int main()
     cout<<"\n*  4. Danh sach sinh vien                             *";
     cout<<"\n*  5. Xoa                                             *";
     cout<<"\n\Nhap tuy chon: ";
-    nguoi ns;
-    ns.nhap_nguoi();
-    ns.xuat_nguoi();
+    cin>>n;
+    switch(n){
+    		case (1):{
+    			cout<<"Nhap so luong sinh vien muon nhap vao: ";
+    			cin>>x;
+    			for( int i = 0; i < x ; i++){
+    				cout<<"\tThong tin sinh vien thu "<<i+1;
+    				ns.nhap_nguoi();
+						}
+    			ns.xuat_nguoi();
+            		}
+            		break;
+            case(2):{
+				ns.timkiem_msv();
+			}		
+	}
+
+
+
 }
 
 
